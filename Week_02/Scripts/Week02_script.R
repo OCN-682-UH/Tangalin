38c44027ecade342fc2f597b05e07e2344825a25
Ainacology <ntangalin@hawaii.edu>
2025-09-07 22:59:09 -1000

    Add folder with subfolders

---
+++ Week_02/Scripts/Week02_script.R
@@ -0,0 +1,18 @@
+### This is my first script.  I am learning how to import data ####
+### Created by: Natalia Tangalin ####
+### Created on: 2025-09-07 ####
+
+
+### libraries #####
+library(tidyverse)
+library(here)
+
+
+### Read in my data #####
+weightData<-read_csv(here("Week_02","Data","weightdata.csv"))
+
+
+# analysis #####
+head(weightData)
+tail(weightData) ## looks at the bottom 6 lines
+view(weightData)
