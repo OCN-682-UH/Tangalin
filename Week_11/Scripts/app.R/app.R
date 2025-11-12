0f647b4bda028583a5528f88e28f27a91f5e3950
Ainacology <ntangalin@hawaii.edu>
2025-11-11 22:57:30 -1000

    DaBest_app

---
+++ Week_11/Scripts/app.R/app.R
@@ -0,0 +1,34 @@
+# This is a Shiny web application.
+# https://ainacology.shinyapps.io/DaBest_app/
+
+library(shiny)
+library(rsconnect)
+
+## Define choices
+Hawaiian_Islands <- c("Niihau", "Kauai", "Oahu", "Molokai", "Maui", "Lanai", "Big Island")
+local_food <- c("spam musubi", "manapua", "saimin", "loco moco", "malasadas", "poke")
+
+# UI
+ui <- fluidPage(
+  titlePanel("Da Best or Broke Da Mouth Hawaiian Survey"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("island", "What's the best Hawaiian Island?", choices = Hawaiian_Islands),
+      radioButtons("food", "What's is da most broke da mouth local food?", choices = local_food)
+    ),
+    mainPanel(
+      textOutput("selection")
+    )
+  )
+)
+
+# Server
+server <- function(input, output) {
+  output$selection <- renderText({
+    paste("You chose", input$island, "as the best island and", input$food, "as your favorite local food.")
+  })
+}
+
+
+# Run the application
+shinyApp(ui = ui, server = server)
